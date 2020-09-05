3facc4a1f4132fac9037c77c5a228948fa5485ca
DiKwickley <sprx077@gmail.com>
2020-09-05 11:09:19 +0530

    added links

--- index.js
+++ index.js
@@ -14,11 +14,12 @@ app.use('/plan', routes)
 app.get('/', (req, res) => {
     res.send(`
         <h1> Hi! This api was made by: ANIKET SINGH RAWAT </h1>
-        <p>Usage: /url/plan/{subject}</p>
+        <p>Usage: https://gate-plan.herokuapp.com/plan/{subject}</p>
         <ul>
-            <li>cn : computer network 1.5 hrs</li>
-            <li>db : database 1 hr</li>
-            <li>pds : Programming and Data structures 1 hr</li>
+            <a href="https://gate-plan.herokuapp.com/plan/cn" > <li>cn : computer network 1.5 hrs</li> </a>
+            <a href="https://gate-plan.herokuapp.com/plan/db" > <li>db : database 1 hr</li> </a>
+            <a href="https://gate-plan.herokuapp.com/plan/pds"> <li>pds : Programming and Data structures 1 hr</li> </a>
+            
         </ul>
     `)
 })
