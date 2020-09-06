8b574d9ade2c2e8697e04273eda3c36f6000f8ad
Aniket Singh Rawat <sprx077@gmail.com>
2020-09-06 15:22:09 +0530

    added route to change status

--- routes.js
+++ routes.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const router = express.Router()
 
-const csv = require('csv-parser');
+const csv = require('csv-parser');  //reading csv
+const fastcsv = require('fast-csv'); //writing csv
 const fs = require('fs');
 
 router.get('/:sub', (req, res) => {
@@ -11,11 +12,50 @@ router.get('/:sub', (req, res) => {
         fs.createReadStream( './subjects/' + req.params.sub + '.csv')
         .pipe(csv())
         .on('data', (row) => {
-            delete row.day
+            
             data.push(row)
         })
         .on('end', () => {
             res.send({'subject' : req.params.sub, 'data': data})
+        });
+
+    } else {
+        res.send('Sorry ' + req.params.sub + ' not available')
+    }
+    
+})
+
+
+router.get('/change/:sub/:index/:status', (req, res) => {
+    subjects = ['cn', 'db', 'pds']
+    data = []
+    if(subjects.includes(req.params.sub)){
+        fs.createReadStream( './subjects/' + req.params.sub + '.csv')
+        .pipe(csv())
+        .on('data', (row) => {
+            
+            data.push(row)
+        })
+        .on('end', () => {
+
+        	
+        	if(req.params.index >= 0 && req.params.index < data.length){
+	        	const ws = fs.createWriteStream('./subjects/' + req.params.sub + '.csv');
+
+	        	data[parseInt(req.params.index)]['status'] = req.params.status 
+	        	
+
+		        	fastcsv
+				  	.write(data, { headers: true })
+				  	.pipe(ws);
+				
+
+			  	// res.send({'subject' : req.params.sub, 'data': data})
+
+	            res.send('status change success')
+	        } else {
+	        	res.send('Incorrect index')
+	        }
         });
 
     } else {
